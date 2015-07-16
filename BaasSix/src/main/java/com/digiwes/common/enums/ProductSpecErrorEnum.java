72a82fa46605c315ab58e4834ab2fdc197c55071
liuwei29 <liuwei29@asiainfo.com>
2015-07-16 17:39:02 +0800

    errorCodeEnum

--- BaasSix/src/main/java/com/digiwes/common/enums/ProductSpecErrorEnum.java
+++ BaasSix/src/main/java/com/digiwes/common/enums/ProductSpecErrorEnum.java
@@ -4,20 +4,22 @@ package com.digiwes.common.enums;
  * Created by huangyq3 on 2015-07-16.
  */
 public enum ProductSpecErrorEnum {
-    PROD_SPEC_NAME_IS_NULL_OR_EMPTY(1001, "name is null or empty"),
-    PROD_SPEC_IS_NULL(1004, "productSpecification is null"),
-    PROD_SPEC_RELATIONSHIP_TYPE_IS_NULL(1003, "relationshipType is null"),
-    PROD_SPEC_EQUALS_TO_CURRENT(1005, "the designated productSpecification equal to current value"),
-    PROD_SPEC_HAS_RELATED_TO_CURRENT(1006, "the designated productSpecification equal to current value"),
-    PROD_SPEC_CHAR_IS_NULL(1007, "Char is null"),
-    PROD_SPEC_CHAR_MAX_LESS_THAN_MAX(1008, "maxCardinality is less than minCardinality"),
-    PROD_SPEC_CHAR_TYPE_DIFFERENT_CHAR_VALUE_TYPE(1009, "The valueType of Character and the valueType of CharacterValue are the same"),
-    PROD_SPEC_CHAR_VALUE_IS_NULL(1010, "charVal is null"),
-    PROD_SPEC_CHAR_VALUE_NOT_BELONG_TO_CHAR(1011, "The charValue do not belong to this char"),
-    PROD_SPEC_CHAR_HAS_NO_CHAR_VALUE(1012, "no charValue under the current char"),
-    PROD_SPEC_CHAR_RELATIONSHIP_TYPE_IS_NULL(1013, "relationshipType is null"),
-    PROD_SPEC_CHAR_EQUALS_TO_CURRENT(1014, "the designated productSpecCharacteristic equal to current value"),
-    PROD_SPEC_CHAR_HAS_RELATED_TO_CURRENT(1015, "Characteristic has been established associate relationship in the specified time");
+    SPEC_NAME_IS_NULL(286064640, "name is null or empty"),
+    PROD_SPEC_IS_NULL(286064641, "productSpecification is null"),
+    PROD_SPEC_RELATIONSHIP_TYPE_IS_NULL(286064642, "product specification relationshipType is null"),
+    PROD_SPEC_EQUALS_TO_CURRENT(286064643, "the designated productSpecification equal to current productSpecification"),
+    PROD_SPEC_HAS_RELATED_TO_CURRENT(286064644, "the designated productSpecification equal to current value"),
+    PROD_SPEC_CHAR_IS_NULL(286064645, "Char is null"),
+    PROD_SPEC_CHAR_MAX_LESS_THAN_MAX(286064646, "maxCardinality is less than minCardinality"),
+    PROD_SPEC_CHAR_TYPE_DIFFERENT_CHAR_VALUE_TYPE(286064647, "The valueType of Character and the valueType of " +
+            "CharacterValue are the same"),
+    PROD_SPEC_CHAR_VALUE_IS_NULL(286064648, "charVal is null"),
+    PROD_SPEC_CHAR_VALUE_NOT_BELONG_TO_CHAR(286064649, "The charValue do not belong to this char"),
+    PROD_SPEC_CHAR_HAS_NO_CHAR_VALUE(286064650, "no charValue under the current char"),
+    PROD_SPEC_CHAR_RELATIONSHIP_TYPE_IS_NULL(286064651, "relationshipType is null"),
+    PROD_SPEC_CHAR_EQUALS_TO_CURRENT(286064652, "the designated productSpecCharacteristic equal to current value"),
+    PROD_SPEC_CHAR_HAS_RELATED_TO_CURRENT(286064653, "Characteristic has been established associate relationship in " +
+            "the specified time");
     private int code;
     private String message;
 
